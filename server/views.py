9847abde612a2448af29d3ac80dad05478b43428
Anomalouss <anomalouss247@gmail.com>
2022-03-11 08:52:35 +0200

    Login endpoint added

--- server/views.py
+++ server/views.py
@@ -3,10 +3,9 @@ import db
 
 views = Blueprint(__name__, "views")
 
-
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 @views.route("/user/register", methods=["POST"])
 def register():
@@ -18,3 +17,7 @@ def register():
         return {"code": 200, "message": "User registered successfully"}
     else:
         return {"code": 400, "message": "User already exists"}
+
+@views.route("/user/login", methods=["GET"])
+def login():
+    pass
