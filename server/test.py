03336c9a0540953e054c4cf0e3e3b3f25b815204
AlexOgn <alexognyanov@abv.bg>
2022-03-11 10:14:11 +0200

    Habibi

--- server/test.py
+++ server/test.py
@@ -1,9 +1,8 @@
 import requests
 
 URL = "http://127.0.0.1:5000/user/login"
-PARAMS = {"username": "Sasho", "password": "123"}
+PARAMS = {"username": "Sasho", "password": "Rikoshet123321"}
 
-r = requests.get(url = URL, params = PARAMS)\
+r = requests.get(url = URL, params = PARAMS)
 
 data = r.json()
-print(data)
